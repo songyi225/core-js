7396fc22d0133f04557f4bd8d70ebb853dd89138
songyi225 <sy920225@naver.com>
2024-05-29 16:11:50 +0900

    js 변수와 상수 그리고 var

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,46 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let name = 'John';
+let admin = name;
+// name에 삭선 그어진 이유?
+// window.name 으로 들어가있음!
+
+console.log(admin);
+
+const ourPlanetName = 'earth';
+let currentOnlineUserName = 'songyi';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기 Calculate product price by quantity
+let productPriceQuantity;
+
+// - 구매 제품 가격의 총 합 Total price of purchased products
+let productTotalPrice;
+
+// - 1년 기준 일(day)자 수 Number of days per year
+const DAY_PER_YEAR = 365;
+
+// - 구매 결제 여부 Whether to pay for a purchase
+// true , false => 보통 변수 이름 앞에 is를 붙임
+// 가지고 있느냐 없느냐 => has를 붙임
+let isPayment = false;
+let hasCartList = false;
+
+// - 구매 결제 내역 Purchase payment history
+let paymentList;
+
+// - 브랜드 접두사 brand prefix
+const BRAND_PREFIX = 'Kurly';
+
+// - 오늘의 운세 today's horoscope
+let fortuneToday = 'so good';
+
+// - 상품 정보 product information
+const productInfomation = '';
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
